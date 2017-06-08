c8d02d4e802831207f7151655bb2717f0654c2ef
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-06-08 18:02:28 +0200

    Real time simulation added

--- Racket Motion Server/main.m
+++ Racket Motion Server/main.m
@@ -8,17 +8,42 @@ clear variables;
 
 %% Begin processing data here:
 % 1) Read IMU data:
-path = '../Offline Sensor Data/test3/';
+
+testName = 'test4';
+path = ['../Offline Sensor Data/', testName, '/'];
 range = [3, inf];
 type = 'float';
 
 dt = 1/100;
-[acc, gyro, mag, t] = readSensData(path,...
-                                   'test3_acc.pcm',...
-                                   'test3_gyro.pcm',...
-                                   'test3_mag.pcm',...
+[acc_dat, gyro_dat, mag_dat, t_dat] = readSensData(path,...
+                                   [testName, '_acc.pcm'],...
+                                   [testName, '_gyro.pcm'],...
+                                   [testName, '_mag.pcm'],...
                                    dt, range, type);
 
-% 2) Plot raw data:
-plotSensData( 'RAW Data', t, acc, gyro, mag);
+% 2) Iterate over data plot it and fit it:
+hFig = figure('Name', 'RAW Data'); 
+% plotSensData(hFig, t_dat, acc_dat, gyro_dat, mag_dat);
+
+t = [];
+acc = [];
+gyro = [];
+mag = [];
+for k=1:length(t_dat)
+    t = [t, t_dat(k)];
+    acc = [acc, acc_dat(:,k)];
+    gyro = [gyro, gyro_dat(:,k)];
+    mag = [mag, mag_dat(:,k)];
+    
+    plotSensData(hFig, t, acc, gyro, mag);
+    
+    pause(dt);
+end
+
+
+
+
+
+
+
 
