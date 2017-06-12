9fe96c53c1c1db0c96e9d0bc5faa79a2de45db52
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-06-12 17:26:44 +0200

    ...

--- Racket Motion Server_Qt/RacketMotionServer/fakeSensorClient/fakeSensorClient.py
+++ Racket Motion Server_Qt/RacketMotionServer/fakeSensorClient/fakeSensorClient.py
@@ -7,7 +7,7 @@ Alexander Kozhinov <AlexanderKozhinov@yandex.com>
 """
 
 # import timeit
-# import numpy as np
+import numpy as np
 # import scipy.stats as scs
 # import matplotlib.pyplot as plt
 import socket
@@ -16,25 +16,35 @@ import socket
 
 
 # -----------------------------------------------------------------------------
-def sendFakeSensorData(ip, port, message):
+def sendFakeSensorData(ip, port, N=100):
     """
     Sends an udp packet as string
 
-    >>> sendToUDP("127.0.0.1", 5555, "hey hey")
+    >>> sendFakeSensorData("127.0.0.1", 5555, 3)
     """
     # initialize socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+
     # send the command
-    s.sendto(message.encode(), (ip, port))
- 
+    del3 = ", 3,  ";
+    del4 = ", 4,  ";
+    del5 = ", 5,  ";
+    delim = ", ";
+    dat = ", 3,  -0.059,  0.098,  9.826, 4,  -0.000,  0.001,  0.001, 5,  16.191, 12.642,-34.497"
+    for k in range(0, N):
+        data = str(k)
+        data = data.join(dat)
+
+        print("data: {", data, "}")
+        s.sendto(data.encode('ascii', 'replace'), (ip, port))
+
     # close the socket
     s.close()
 
 
 # -----------------------------------------------------------------------------
 def main():
-    sendToUDP("127.0.0.1", 5555, "hey hey")
+    sendFakeSensorData("127.0.0.1", 5555, N=3)
 
 
 # -----------------------------------------------------------------------------
