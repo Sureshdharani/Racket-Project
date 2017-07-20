a0169c216baea4300144ea15a141df6262e3ec87
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-07-20 21:32:01 +0200

    RacketPy Project created

---
+++ RacketPy/RacketPy.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2017
+RacketPy
+Alexander Kozhinov <AlexanderKozhinov@yandex.com>
+"""
+
+# import numpy as np
+# import argparse
+
+
+# -----------------------------------------------------------------------------
+def tmp():
+    """
+    Temporary function
+    """
+    pass
+
+
+# -----------------------------------------------------------------------------
+def main():
+    """
+    Main function.
+
+    # >>> main()
+    """
+    pass
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
